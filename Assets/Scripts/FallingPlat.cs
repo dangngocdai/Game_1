cca88ac4a972b234f52221b57a2d2191a823f772
Đặng Ngọc Đại <forever.one.love.myapple@gmail.com>
2019-11-22 19:04:05 +0700

    update lan 3

---
+++ Assets/Scripts/FallingPlat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlat : MonoBehaviour
+{
+
+    public Rigidbody2D r2;
+    public float timedelay = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,53 +23,59 @@ public class Player : MonoBehaviour
         anim.SetFloat("Speed", Mathf.Abs(r2.velocity.x)); // gán giá trị Speed trong animator bằng giá trị thật của tốc độ(max 3) 
         //  Mathf.Abs(r2.velocity.x) trị tuyệt đối
 
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             if (grounded)
-            grounded = false;
-            doublejump = true;
-            r2.AddForce(Vector2.up * jumpPow);
-        }
-        else
-        {
-            if (doublejump)
             {
-                doublejump = false;
-                r2.velocity = new Vector2(r2.velocity.x, 0);
-                r2.AddForce(Vector2.up * jumpPow * 0.7f);
+                doublejump = true;
+                grounded = false;
+                r2.AddForce(Vector2.up * jumpPow);
+            }
+            else
+            {
+                if (doublejump)
+                {
+                    doublejump = false;
+                    r2.velocity = new Vector2(r2.velocity.x, 0);
+                    r2.AddForce(Vector2.up * jumpPow * 0.7f);
+                }
             }
         }
     }
 
-    void FixedUpdate()// tương tự như Update() nhưng sẽ đc update 0.2s và dính lứu nhiều hơn đến vật lý hơn là fun Update
-    {
-        float h = Input.GetAxis("Horizontal");// lấy thuộc tính Horizontal trong Input
-        r2.AddForce((Vector2.right) * speed * h);
-
-        if (r2.velocity.x > maxspeed)
-            r2.velocity = new Vector2(maxspeed, r2.velocity.y);// giới hạn tốc độ khi về phía bên phải
-
-        if (r2.velocity.x < -maxspeed)
-            r2.velocity = new Vector2(-maxspeed, r2.velocity.y);// giới hạn tốc độ khi về phía bên trái
-
-        if (h > 0 && !faceright)
+        void FixedUpdate()// tương tự như Update() nhưng sẽ đc update 0.2s và dính lứu nhiều hơn đến vật lý hơn là fun Update
         {
-            Flip();
+            float h = Input.GetAxis("Horizontal");// lấy thuộc tính Horizontal trong Input
+            r2.AddForce((Vector2.right) * speed * h);
+
+            if (r2.velocity.x > maxspeed)
+                r2.velocity = new Vector2(maxspeed, r2.velocity.y);// giới hạn tốc độ khi về phía bên phải
+
+            if (r2.velocity.x < -maxspeed)
+                r2.velocity = new Vector2(-maxspeed, r2.velocity.y);// giới hạn tốc độ khi về phía bên trái
+
+            if (h > 0 && !faceright)
+            {
+                Flip();
+            }
+
+            if (h < 0 && faceright)
+            {
+                Flip();
+            }
+        if (grounded)
+        {
+            r2.velocity = new Vector2(r2.velocity.x * 0.7f, r2.velocity.y); //khi mà nv chạm đất thì tạo lực ma sát(giảm velocity 0.7 lan)
+        }
         }
 
-        if (h < 0 && faceright)
+        void Flip()
         {
-            Flip();
+            faceright = !faceright;
+            Vector3 Scale;
+            Scale = transform.localScale;
+            Scale.x *= -1;
+            transform.localScale = Scale;
+
         }
     }
-
-    void Flip()
-    {
-        faceright = !faceright;
-        Vector3 Scale;
-        Scale = transform.localScale;
-        Scale.x *= -1;
-        transform.localScale = Scale;
-
-    }
-}
